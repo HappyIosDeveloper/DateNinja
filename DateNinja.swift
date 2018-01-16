ca44027d6d33fcdd6ad09a4ad7162b54c649dc8c
Alfredo <lovlyboy@gmail.com>
2018-01-16 16:26:09 +0330

    Update DateNinja.swift
    Small bug fix

--- DateNinja.swift
+++ DateNinja.swift
@@ -1,6 +1,6 @@
 //
 //  DateNinja.swift
-//  flightio
+//  WhatEVER
 //
 //  Created by GCO2(Ahmadreza) on 1/16/18.
 //  Copyright © 2018 Alfredo Uzumaki. All rights reserved.
@@ -10,7 +10,7 @@ import UIKit
 
 /*
  USAGE
- print(String(describing: toDateNinja("2017-10-20 15:40:00"))) // GMT
+ print(String(describing: "2017-10-20 15:40:00".toDateNinja())) // GMT
  
  CAUTION
  in case of failure, this date will be return => "1999-09-09 09:09:09"
@@ -34,10 +34,10 @@ extension String {
         return a + " 00:00:00"
     }
     
-    func toDateNinja(_ dateString: String, _ resetTime: Bool = false)-> Date {
-        let stringWithoutPlus = dateString.removeAfter("+")
+    func toDateNinja(_ resetTime: Bool = false)-> Date {
+        let stringWithoutPlus = self.removeAfter("+")
         let stringWithoutDot = stringWithoutPlus.removeAfter(".")
-        var std = stringWithoutDot.replace("T", with:" ").replace("/", with: "-")
+        var std = stringWithoutDot.replace("T", with:" ").replace("/", with: "-") // for dates like this: 2020/10/10
         
         let dateFormatterGet = DateFormatter()
         dateFormatterGet.dateFormat = "yyyy-MM-dd hh:mm:ss" // regular Date
@@ -81,7 +81,7 @@ extension String {
             dateFormatterGet.dateFormat = "yyyy-MM-dd hh:mm:ssZ" // regular Date
             return dateFormatterGet.date(from: String(describing: a))!
         }
-        print(" *********** ==> COULC NOT CONVERT THIS DATE: \(dateString) <== *********** ")
+        print(" *********** ==> COULC NOT CONVERT THIS DATE: \(self) <== *********** ")
         dateFormatterGet.dateFormat = "yyyy-MM-dd hh:mm:ss"
         return dateFormatterGet.date(from: "1999-09-09 09:09:09" )! // default date! returns if none of above convers was successful
     }
